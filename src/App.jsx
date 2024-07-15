2fc1d2e91236f7e2d8e62f138cf43f6d5b103e59
Anik0047 <clananik@gmail.com>
2024-07-15 20:12:35 +0600

    sass install

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <h2>Hello</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi omnis cum quia cupiditate eligendi quasi nemo nulla nihil! Nisi eos odit sapiente id optio sit dolor tenetur modi consequatur maxime. Dicta nostrum distinctio voluptatum et beatae itaque aspernatur praesentium. Laboriosam sed similique incidunt necessitatibus officiis tempore, quam excepturi neque maxime a optio quo sint distinctio reprehenderit blanditiis dolorem. Aliquam officia dolore officiis a, soluta praesentium velit non unde minus deserunt iure illum quas libero, sint exercitationem! Quos, saepe ducimus ratione similique nemo aut quae earum ab excepturi! Ducimus dicta nemo earum sint, voluptatibus, distinctio quasi nisi voluptatum inventore, impedit quidem quis expedita! Nulla necessitatibus accusamus modi voluptatibus nam, eius velit a corrupti mollitia omnis animi esse est officia eaque laudantium ut cum aspernatur! Quam possimus corrupti itaque explicabo ab optio, voluptates ea facilis laudantium ratione voluptatibus libero ducimus rerum tenetur quidem minus adipisci! Ad eligendi reprehenderit maxime ullam recusandae. Accusantium in porro maxime minima, ea inventore, fugiat minus, quibusdam nostrum earum magnam omnis cum aliquid incidunt repellat non nulla animi at provident illo fuga. Error nostrum delectus accusantium excepturi odio pariatur quas repudiandae nesciunt animi, sed cum fuga quisquam nobis repellat minima possimus earum, iure dignissimos commodi molestiae! Non!
     </div>
   )
 }
