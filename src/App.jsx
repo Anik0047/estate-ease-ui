db9ed5ae7718282b959b8fd7ff64619028a8512c
Anik0047 <clananik@gmail.com>
2024-07-16 16:26:01 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar/Navbar"
 import "./layout.scss"
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <div className="layout">
-      hello world
+      <Navbar></Navbar>
     </div>
   )
 }
