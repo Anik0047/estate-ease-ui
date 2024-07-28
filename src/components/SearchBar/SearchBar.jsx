1bb32a7102566515abf159425dcbce92e351c0c6
Anik0047 <clananik@gmail.com>
2024-07-28 22:18:50 +0600

    done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -20,7 +20,23 @@ function SearchBar(){
     }
 
   return (
-    
+    <div className='searchBar'>
+        <div className="type">
+            {types.map((type) => (
+                <button key={type} onClick={ () => switchType(type)} className={query.type === type ? "active" : ""}>
+                    {type}
+                </button>
+            ))}
+        </div>
+            <form>
+                <input type="text" name='location' placeholder='City Location'/>
+                <input type="number" name='minPrice' min={0} max={10000000} placeholder='Min Price'/>
+                <input type="number" name='maxPrice' min={0} max={10000000} placeholder='Max Price'/>
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
+            </form>
+    </div>
   )
 }
 
