f85e9d69c93fcc6d1cf00290a6775434b8b60393
Anik0047 <clananik@gmail.com>
2024-07-29 13:28:32 +0600

    making cards

--- src/Routes/ListPage/ListPage.jsx
+++ src/Routes/ListPage/ListPage.jsx
@@ -1,8 +1,26 @@
+import Filter from '../../components/Filter/Filter';
+import Card from '../../components/Card/Card'
+import { listData } from '../../lib/dummydata'
 import './ListPage.scss'
 
 function ListPage(){
+
+  const data = listData;
+
   return (
-    <div className='listPage'>ListPage</div>
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {
+            data.map(item=>(
+              <Card key={item.id}/>
+            ))
+          }
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>  
+    </div>
   )
 }
 
