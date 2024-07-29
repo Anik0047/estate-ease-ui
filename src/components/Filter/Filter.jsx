71dbc6321da613ccf57d20c1a69b22653400e48c
Anik0047 <clananik@gmail.com>
2024-07-29 19:10:58 +0600

    working in card filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,8 +2,19 @@ import './Filter.scss'
 
 function Filter(){
   return (
-    <div className='filter'>Filter</div>
+    <div className='filter'>
+      <h1>Search results for <b>London</b></h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input type="text" id='city' name='city' placeholder='City Location'/>
+        </div>
+      </div>
+      <div className="bottom">
+        
+      </div>
+    </div>
   )
 }
 
-export default Filter
+export default Filter;
