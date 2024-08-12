593330b75672baf29deb94840c7858bd34003af3
Anik0047 <clananik@gmail.com>
2024-08-12 19:06:40 +0600

    slider working

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,8 +1,17 @@
 import './Slider.scss'
 
-function Slider(){
+function Slider({images}){
 return (
-    <div className='slider'>Slider</div>
+    <div className='slider'>
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image,index) => (
+          <img src={image} key={index}/>
+        ))}
+      </div>
+    </div>
   )
 }
 export default Slider
