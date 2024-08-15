f3cf80ad62508f01b34bf25df1c578268b32699f
Anik0047 <clananik@gmail.com>
2024-08-15 23:03:59 +0600

    working in the profile page

--- src/Routes/ProfilePage/ProfilePage.jsx
+++ src/Routes/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,35 @@
+import List from '../../components/List/List'
 import './ProfilePage.scss'
 
 function ProfilePage(){
 return (
-    <div className='profilePage'>ProfilePage</div>
+    <div className='profilePage'>
+        <div className="details">
+            <div className="wrapper">
+                <div className="title">
+                    <h1>User Infomation</h1>
+                    <button>Update Profile</button>
+                </div>
+                <div className="info">
+                    <span>Avatar: <img src="/profile.png" alt="" /></span>
+                    <span>Username: <b>Anik Barua</b></span>
+                    <span>E-mail: <b>anik@gmail.com</b></span>
+                </div>
+                <div className="title">
+                    <h1>My List</h1>
+                    <button>Create New Post</button>
+                </div>
+                <List />
+                <div className="title">
+                    <h1>Saved List</h1>
+                </div>
+                <List/>
+            </div>
+        </div>
+        <div className="chatContainer">
+            <div className="wrapper"></div>
+        </div>
+    </div>
   )
 }
 export default ProfilePage
